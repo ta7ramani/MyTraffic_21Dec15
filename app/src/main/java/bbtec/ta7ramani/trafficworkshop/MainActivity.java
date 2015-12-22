d325d0a06ef24ca79c634579d41eecc834baf84a
charatroek <ta7ramani@gmail.com>
2015-12-22 10:26:33 +0700

    buttonController เสร็จแล้ว

--- app/src/main/java/bbtec/ta7ramani/trafficworkshop/MainActivity.java
+++ app/src/main/java/bbtec/ta7ramani/trafficworkshop/MainActivity.java
@@ -1,13 +1,59 @@
 package bbtec.ta7ramani.trafficworkshop;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแกร
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+
+    }   //Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.sheep);
+                buttonMediaPlayer.start();
+                //https://youtu.be/unU8SdLtd3E
+
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/unU8SdLtd3E"));
+                startActivity(objIntent);
+
+
+            }   //event
+        });
+
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+
+
+}   // Main Class
